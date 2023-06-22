3beda00b51924cc12e3e88cd4cb1f777bb653a09
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-22 18:34:26 -0300

    got the grid fixed and the pixels too

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let body = document.querySelector('body');
 let outer_div = document.createElement('div');
 
-outer_div.style.cssText = "display: flex; justify-content: space-between; flex-wrap: wrap; margin: 40px 350px; background-color: black";
+outer_div.style.cssText = "display: flex; flex-wrap: wrap; border: black 10px solid; width: 457.7px; height: 458.7px; margin: 12% 35%";
 for (let index = 0; index < 256; index++) {
     let innerdiv = document.createElement('div');
-    innerdiv.style.cssText = "background-color: white; margin: 10px; width: 30px; height: 30px";
+    innerdiv.style.cssText = "background-color: white; width: 27px; height: 27px; border: black 0.5px solid; ";
     outer_div.appendChild(innerdiv);
     innerdiv.addEventListener('mouseover', e =>{
         innerdiv.style.backgroundColor = 'blue';
