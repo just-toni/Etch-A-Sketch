7257a5d14e995685c1ce5f9e5316d94c671b8142
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-22 18:00:46 -0300

    added hover effect for each grid

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let outer_div = document.createElement('div');
 outer_div.style.cssText = "display: flex; justify-content: space-between; flex-wrap: wrap; margin: 40px 350px; background-color: black";
 for (let index = 0; index < 256; index++) {
     let innerdiv = document.createElement('div');
-    // innerdiv.textContent = "I am a div";
-    // innerdiv.style.backgroundColor = "blue";
     innerdiv.style.cssText = "background-color: white; margin: 10px; width: 30px; height: 30px";
     outer_div.appendChild(innerdiv);
+    innerdiv.addEventListener('mouseover', e =>{
+        innerdiv.style.backgroundColor = 'blue';
+    })
 }
 body.appendChild(outer_div);
