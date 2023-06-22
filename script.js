1fb6b3ebe292bc43896ccb382f6c55de63f4ed28
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-22 17:55:41 -0300

    created 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let body = document.getElementsByName('body');
+let body = document.querySelector('body');
 let outer_div = document.createElement('div');
+
+outer_div.style.cssText = "display: flex; justify-content: space-between; flex-wrap: wrap; margin: 40px 350px; background-color: black";
+for (let index = 0; index < 256; index++) {
+    let innerdiv = document.createElement('div');
+    // innerdiv.textContent = "I am a div";
+    // innerdiv.style.backgroundColor = "blue";
+    innerdiv.style.cssText = "background-color: white; margin: 10px; width: 30px; height: 30px";
+    outer_div.appendChild(innerdiv);
+}
+body.appendChild(outer_div);
