278340e67b04869d3b3e7c2e00188eb86c52a9f3
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-26 19:11:50 -0300

    made progress with the aside section, trying to arrange the aside section and ensure the grid doesn't change as well. Also set a default value for the slider when the page loads

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let body = document.querySelector('body');
-let outer_div = document.createElement('div');
+let main = document.createElement('main');
+let aside = document.createElement('aside');
+body.style.cssText = "display: flex; flex-direction: row;";
+// main.style.cssText = "width: 70%";
+main.style.cssText = "display: flex; flex-wrap: wrap; border: black 10px solid; width: 457.7px; height: 458.7px;";
+aside.style.cssText = "width: 30%; display: flex; flex-direction: column";
+
+let randomColourBtn = document.createElement('button');
+aside.appendChild(randomColourBtn);
+let defaultColourBtn = document.createElement('button');
+aside.appendChild(defaultColourBtn);
+let clearButton = document.createElement('button');
+aside.appendChild(clearButton);
+let eraserBtn = document.createElement('button');
+aside.appendChild(eraserBtn);
+body.appendChild(aside)
+let textSizeControl = document.createElement('p');
+textSizeControl.textContent = `16 x 16`;
+aside.appendChild(textSizeControl);
 let buttonsldercontainer = document.createElement('section');
 let buttonslider = document.createElement('input');
-let textSizeControl = document.createElement('p');
-body.appendChild(textSizeControl);
-let aside = document.createElement('aside');
-let randomColourBtn = document.createElement('button');
-let defaultColourBtn = document.createElement('button');
-let clearButton = document.createElement('button');
-let eraserBtn = document.createElement('button');
 buttonslider.setAttribute('type', 'range');
 buttonslider.setAttribute('min', '1');
 buttonslider.setAttribute('max', '100');
@@ -18,27 +29,26 @@ function getValue(e){
 }
 buttonslider.style.cssText = "width: 100px; height: 25px; background: grey; outline: none; opacity: 0.7;";
 buttonsldercontainer.appendChild(buttonslider);
-body.appendChild(buttonslider);
+aside.appendChild(buttonslider);
 
-outer_div.style.cssText = "display: flex; flex-wrap: wrap; border: black 10px solid; width: 457.7px; height: 458.7px; margin: 12% 35%";
 for (let index = 0; index < 256; index++) {
-    let innerdiv = document.createElement('div');
-    innerdiv.style.cssText = "background-color: white; width: 27px; height: 27px; border: black 0.5px solid; ";
-    outer_div.appendChild(innerdiv);
-    innerdiv.addEventListener('mouseover', e =>{
-        innerdiv.style.backgroundColor = 'blue';
+    let innerSection = document.createElement('section');
+    innerSection.style.cssText = "background-color: white; width: 27px; height: 27px; border: black 0.5px solid; ";
+    main.appendChild(innerSection);
+    innerSection.addEventListener('mouseover', e =>{
+        innerSection.style.backgroundColor = 'blue';
     });
-    // innerdiv.addEventListener('mouseout', e =>{
-    //     innerdiv.style.backgroundColor = 'white';
+    // innerSection.addEventListener('mouseout', e =>{
+    //     innerSection.style.backgroundColor = 'white';
     // })
 }
 
 // for (let index = 0; index < 4096; index++) {
-//     let innerdiv = document.createElement('div');
-//     innerdiv.style.cssText = "background-color: white; width: 7px; height: 7px; border: black 0.5px solid; ";
-//     outer_div.appendChild(innerdiv);
-//     innerdiv.addEventListener('mouseover', e =>{
-//         innerdiv.style.backgroundColor = 'blue';
+//     let innerSection = document.createElement('div');
+//     innerSection.style.cssText = "background-color: white; width: 7px; height: 7px; border: black 0.5px solid; ";
+//     main.appendChild(innerSection);
+//     innerSection.addEventListener('mouseover', e =>{
+//         innerSection.style.backgroundColor = 'blue';
 //     })
 // }
-body.appendChild(outer_div);
+body.appendChild(main);
