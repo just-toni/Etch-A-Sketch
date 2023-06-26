648ec9cced9b83b6d469a303c4d488b39544d222
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-26 18:35:21 -0300

    added a slider to chnage the number of squares the grod would contain but still trying to figure out how to create a perfect grid for each input entered

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let body = document.querySelector('body');
 let outer_div = document.createElement('div');
+let buttonsldercontainer = document.createElement('section');
+let buttonslider = document.createElement('input');
+buttonslider.setAttribute('type', 'range');
+buttonslider.setAttribute('min', '1');
+buttonslider.setAttribute('max', '100');
+buttonslider.style.cssText = "width: 100px; height: 25px; background: grey; outline: none; opacity: 0.7;";
+console.log(buttonslider.value);
+buttonsldercontainer.appendChild(buttonslider);
+body.appendChild(buttonslider);
 
 outer_div.style.cssText = "display: flex; flex-wrap: wrap; border: black 10px solid; width: 457.7px; height: 458.7px; margin: 12% 35%";
 for (let index = 0; index < 256; index++) {
@@ -8,6 +17,18 @@ for (let index = 0; index < 256; index++) {
     outer_div.appendChild(innerdiv);
     innerdiv.addEventListener('mouseover', e =>{
         innerdiv.style.backgroundColor = 'blue';
-    })
+    });
+    // innerdiv.addEventListener('mouseout', e =>{
+    //     innerdiv.style.backgroundColor = 'white';
+    // })
 }
+
+// for (let index = 0; index < 4096; index++) {
+//     let innerdiv = document.createElement('div');
+//     innerdiv.style.cssText = "background-color: white; width: 7px; height: 7px; border: black 0.5px solid; ";
+//     outer_div.appendChild(innerdiv);
+//     innerdiv.addEventListener('mouseover', e =>{
+//         innerdiv.style.backgroundColor = 'blue';
+//     })
+// }
 body.appendChild(outer_div);
